842fb367dc57e600be45c606414ecf74ec6fd369
kado-yasuyuki <kado-yasuyuki@meleap.com>
2018-01-10 02:23:26 +0900

    implement sample assetbundle

---
+++ GAssetBundleManager/Assets/Sample/Scripts/Sample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace GAssetBundle
+{
+    public class Sample : MonoBehaviour
+    {
+        [SerializeField]
+        string assetBundleServerUrl;
+
+        void Start()
+        {
+            AssetBundleManager.Instance.ConnectToServer(assetBundleServerUrl).Subscribe(_ => OnConnected());
+        }
+
+        void OnConnected()
+        {
+            AssetBundleManager.Instance.GetAsset<GameObject>("cube", "Cube")
+                              .Subscribe(go => Instantiate(go));
+
+        }
+    }
+}
